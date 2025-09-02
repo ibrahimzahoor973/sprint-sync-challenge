296bfa3f3003ab8b67f3451476d8a983c2460307
Ibrahim Zahoor <ibrahimzahoor973@gmail.com>
2025-09-02 14:46:39 +0500

    prisma migration files added + docker compose updated

---
+++ prisma/migrations/20250830032044_insert-admin-user/migration.sql
@@ -0,0 +1,7 @@
+INSERT INTO "users" ("id", "email", "passwordHash", "isAdmin")
+VALUES (
+  'ckj1q1q1q000000',
+  'admin@sprintsync.com',
+  '$2b$10$CwTycUXWue0Thq9StjUM0uJ8vKsPC1z9puepZ8Y0R12/8WmHd6hVO',
+  TRUE
+);
