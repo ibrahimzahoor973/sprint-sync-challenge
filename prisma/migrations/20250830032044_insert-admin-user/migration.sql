a92dace70d7391114278e989be82ef0174967f47
Ibrahim Zahoor <ibrahimzahoor973@gmail.com>
2025-09-02 16:49:13 +0500

    swagger docs + migration files updates

--- prisma/migrations/20250830032044_insert-admin-user/migration.sql
+++ prisma/migrations/20250830032044_insert-admin-user/migration.sql
@@ -1,7 +1,10 @@
-INSERT INTO "users" ("id", "email", "passwordHash", "isAdmin")
+--- Insert an admin user into the users table with password 'admin123'
+INSERT INTO "users" ("id", "email", "passwordHash", "isAdmin", "createdAt","updatedAt")
 VALUES (
   'ckj1q1q1q000000',
   'admin@sprintsync.com',
-  '$2b$10$CwTycUXWue0Thq9StjUM0uJ8vKsPC1z9puepZ8Y0R12/8WmHd6hVO',
-  TRUE
+  '$2b$12$vej.vN.CJOSbd3WrWKiCNuXG3uJOtgguua.sQzSYk3rXbuvzqps4a',
+  TRUE,
+  NOW(),
+  NOW()
 );
